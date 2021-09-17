e5ccd140db69b941c46ad37d3676164df85d8f55
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2021-09-16 21:44:52 -0400

    Created Base Server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("Hello World");
+//constants
+const port = process.env.PORT || 3000;
+
+console.log("Loading Libraries");
+
+import express from "express";
+import http from 'http';
+
+console.log("Starting Server");
+
+const app = express();
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+});
